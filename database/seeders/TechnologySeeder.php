b5d1eee2911394c252890f2572e7866b375ee259
DaniAm94 <dani.amico94@gmail.com>
2024-03-26 16:11:54 +0100

    updated technology seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['label' => 'HTML', 'color' => 'danger'],
+            ['label' => 'CSS', 'color' => 'info'],
+            ['label' => 'ES6', 'color' => 'warning'],
+            ['label' => 'Bootstrap', 'color' => 'dark'],
+            ['label' => 'VUE', 'color' => 'success'],
+            ['label' => 'SQL', 'color' => 'secondary'],
+            ['label' => 'PHP', 'color' => 'info'],
+            ['label' => 'Laravel', 'color' => 'danger'],
+        ];
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->fill($technology);
+            $new_technology->save();
+        }
     }
 }
