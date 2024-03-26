44e595e4f60fd0a672e1d4422d4cec679880d495
DaniAm94 <dani.amico94@gmail.com>
2024-03-26 18:18:30 +0100

    updated guest's views to show projects' technologies

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -18,27 +18,45 @@
 
                 <div class="card my-5">
                     <div class="card-header d-flex justify-content-between align-items-center ">
+                        {{-- Type --}}
                         @if ($project->type)
                             <span class="badge"
                                 style="background-color: {{ $project->type->color }};">{{ $project->type->label }}</span>
                         @else
                             <span class="badge text-bg-secondary ">Nessuna categoria</span>
                         @endif
+
+                        {{-- Pulsante vedi dettaglio --}}
                         <a href="{{ route('guest.projects.show', $project->slug) }}" class="btn btn-sm btn-primary ">Vedi</a>
                     </div>
                     <div class="card-body">
                         <div class="row">
+
+                            {{-- Immagine --}}
                             @if ($project->image)
                                 <div class="col-3">
                                     <img class="img-fluid" src="{{ $project->printImage() }}" alt="{{ $project->title }}">
                                 </div>
                             @endif
                             <div class="col">
+
+                                {{-- Titolo --}}
                                 <h5 class="card-title mb-2 text-break">{{ $project->title }}</h5>
+
+                                {{-- Data creazione --}}
                                 <small class="card-subtitle text-body-secondary">
                                     {{ $project->getFormattedDate('created_at') }}
                                 </small>
+
+                                {{-- Descrizione --}}
                                 <p class="card-text mt-2">{{ $project->getAbstract(70) }}</p>
+
+                                {{-- Tecnologie --}}
+                                @forelse ($project->technologies as $technology)
+                                    <span
+                                        class="badge rounded-pill text-bg-{{ $technology->color }}">{{ $technology->label }}</span>
+                                @empty
+                                @endforelse
                             </div>
                         </div>
                     </div>
--- resources/views/guest/projects/show.blade.php
+++ resources/views/guest/projects/show.blade.php
@@ -5,23 +5,40 @@
 @section('content')
     <div class="card my-5">
         <div class="card-header d-flex justify-content-between align-items-center ">
+            {{-- Type --}}
             @if ($project->type)
                 <span class="badge" style="background-color: {{ $project->type->color }};">{{ $project->type->label }}</span>
             @else
                 <span class="badge text-bg-secondary ">Nessuna categoria</span>
             @endif
+
+            {{-- Pulsante per tornare alla lista --}}
             <a href="{{ route('guest.home') }}" class="btn btn-sm btn-primary ">Torna alla lista</a>
         </div>
         <div class="card-body">
             <div class="row">
+
+                {{-- Immagine --}}
                 @if ($project->image)
                     <div class="col-3">
                         <img class="img-fluid" src="{{ $project->printImage() }}" alt="{{ $project->title }}">
                     </div>
                 @endif
                 <div class="col">
-                    <h5 class="card-title mb-4 ">{{ $project->title }}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $project->getFormattedDate('created_at') }}</h6>
+
+                    {{-- Titolo --}}
+                    <h5 class="card-title ">{{ $project->title }}</h5>
+
+                    {{-- Tecnologie --}}
+                    @forelse ($project->technologies as $technology)
+                        <span class="badge rounded-pill text-bg-{{ $technology->color }}">{{ $technology->label }}</span>
+                    @empty
+                    @endforelse
+
+                    {{-- Data creazione --}}
+                    <h6 class="card-subtitle my-2 text-body-secondary">{{ $project->getFormattedDate('created_at') }}</h6>
+
+                    {{-- Descrizione --}}
                     <p class="card-text">{{ $project->description }}</p>
                 </div>
             </div>
