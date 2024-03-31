a2840b05be486f9083757e750e7cb228801f5643
DaniAm94 <dani.amico94@gmail.com>
2024-03-31 18:06:37 +0200

    added query scope per status into home controller

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     public function __invoke()
     {
         // Nella home sono visibili solo i progetti completati
-        $projects = Project::whereIsCompleted(true)->orderByDesc('created_at')->paginate(6);
+        $projects = Project::statusFilter('completed')->orderByDesc('created_at')->paginate(6);
         return view('guest.home', compact('projects'));
     }
 }
